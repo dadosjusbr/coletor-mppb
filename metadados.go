a51540f178ac07e238e860531a17c8f8ae443335
João Menezes <joao.menezes@ccc.ufcg.edu.br>
2021-10-18 15:13:53 -0300

    Update metadados.go
    Melhor uso do Enum
    
    Co-authored-by: Daniel Fireman <danielfireman@gmail.com>

--- metadados.go
+++ metadados.go
@@ -5,20 +5,18 @@ import (
 )
 
 func Metadados() (coleta.Metadados) {
-	var metadados coleta.Metadados
-
-	metadados.NaoRequerLogin = true
-	metadados.NaoRequerCaptcha = true
-	metadados.Acesso = coleta.Metadados_FormaDeAcesso(1)
-	metadados.Extensao = coleta.Metadados_Extensao(1)
-	metadados.EstritamenteTabular = false
-	metadados.FormatoConsistente = true
-	metadados.TemMatricula = true
-	metadados.TemLotacao = true
-	metadados.TemCargo = true
-	metadados.ReceitaBase = coleta.Metadados_OpcoesDetalhamento(2)
-	metadados.Despesas = coleta.Metadados_OpcoesDetalhamento(2)
-	metadados.OutrasReceitas = coleta.Metadados_OpcoesDetalhamento(2)
-
-	return metadados
+	return coleta.Metadados {
+	    NaoRequerLogin: true,
+	    NaoRequerCaptcha: true,
+	     Acesso: coleta.Metadados_AMIGAVEL_PARA_RASPAGEM,
+	     Extensao: coleta.Metadados_ODS,
+	     EstritamenteTabular:  false,
+	    FormatoConsistente: true,
+	    TemMatricula: true,
+	    TemLotacao:  true,
+	    TemCargo:  true,
+	    ReceitaBase: coleta.Metadados_DETALHADO,
+	    Despesas: coleta.Metadados_DETALHADO,
+	    OutrasReceitas: coleta.Metadados_DETALHADO,
+	}
 }
